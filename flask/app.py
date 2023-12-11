4a4a4f379c668aed1b77fddbfc5cb2ea46d78c30
saravananibus <152783206+saravananibus@users.noreply.github.com>
2023-12-11 12:57:22 +0530

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,6 @@
 import pymysql
 
-app = Flask(__name__)
+app = flask(__name__)
 
 # Replace these with your database credentials
 DB_HOST = 'mydbinstance.ciklgdejnsgr.ap-southeast-1.rds.amazonaws.com'
