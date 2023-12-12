a120635a87af294dcb6ef97b9093cfe867241eb7
saravananibus <152783206+saravananibus@users.noreply.github.com>
2023-12-12 18:52:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-import os
 from flask import Flask, render_template, request
-import pymysql
+import boto3
+from datetime import datetime
 
 app = Flask(__name__)
 
-# Replace these with your database credentials
-DB_HOST = 'saravanandb.c5fkzyuzm8r5.ap-southeast-1.rds.amazonaws.com'
-DB_USER = 'saravanan'
-DB_PASSWORD = 'saravanan1234'
-DB_NAME = "sarandb"
+# Replace these with your AWS credentials and DynamoDB table name
+AWS_ACCESS_KEY = 'AKIAWCVXQWV2MPRZ55GG'
+AWS_SECRET_KEY = 'KCzHbWTZ2RHmlYTMUK5b6mMTPXPgTTfaAJwSyoTz'
+AWS_REGION = 'ap-southeast-1'
+DYNAMODB_TABLE_NAME = 'sarandb'
+
+# Initialize DynamoDB client
+dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION, aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
+
+# Get reference to your DynamoDB table
+table = dynamodb.Table(DYNAMODB_TABLE_NAME)
 
 @app.route('/')
 def index():
@@ -17,39 +23,28 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     username = request.form['username']
-    dob = request.form['dob']
+    dob_str = request.form['dob']
 
-    # Connect to the database
-    connection = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, db=DB_NAME)
+    # Convert dob_str to datetime
+    dob = datetime.strptime(dob_str, '%Y-%m-%d')
 
-    try:
-        with connection.cursor() as cursor:
-            # Insert data into the database
-            sql = "INSERT INTO user_data (username, dob) VALUES (%s, %s)"
-            cursor.execute(sql, (username, dob))
-        connection.commit()
-        return "Data saved successfully"
-    except Exception as e:
-        return str(e)
-    finally:
-        connection.close()
+    # Save data to DynamoDB
+    response = table.put_item(
+        Item={
+            'Username': username,
+            'DOB': dob.strftime('%Y-%m-%d')
+        }
+    )
+
+    return "Data saved successfully"
 
 @app.route('/view')
 def view():
-    # Connect to the database
-    connection = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, db=DB_NAME)
+    # Retrieve data from DynamoDB
+    response = table.scan()
+    data = response.get('Items', [])
 
-    try:
-        with connection.cursor() as cursor:
-            # Retrieve data from the database
-            sql = "SELECT * FROM user_data"
-            cursor.execute(sql)
-            result = cursor.fetchall()
-        return render_template('view.html', data=result)
-    except Exception as e:
-        return str(e)
-    finally:
-        connection.close()
+    return render_template('view.html', data=data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
