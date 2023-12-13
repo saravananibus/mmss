da63d0abf4e3838fa32bec70190ad8fdefb11f6e
saravananibus <152783206+saravananibus@users.noreply.github.com>
2023-12-13 14:59:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 from flask import Flask, render_template, request
-import boto3
-from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'your_rds_database_uri'  # Replace with your RDS database URI
+db = SQLAlchemy(app)
 
-# Replace these with your AWS credentials and DynamoDB table name
-AWS_ACCESS_KEY = 'AKIAWCVXQWV2MPRZ55GG'
-AWS_SECRET_KEY = 'KCzHbWTZ2RHmlYTMUK5b6mMTPXPgTTfaAJwSyoTz'
-AWS_REGION = 'ap-southeast-1'
-DYNAMODB_TABLE_NAME = 'sarandb'
-
-# Initialize DynamoDB client
-dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION, aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
-
-# Get reference to your DynamoDB table
-table = dynamodb.Table(DYNAMODB_TABLE_NAME)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    dob = db.Column(db.String(10), nullable=False)
 
 @app.route('/')
 def index():
@@ -22,29 +16,15 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    username = request.form['username']
-    dob_str = request.form['dob']
+    if request.method == 'POST':
+        username = request.form['username']
+        dob = request.form['dob']
 
-    # Convert dob_str to datetime
-    dob = datetime.strptime(dob_str, '%Y-%m-%d')
+        new_user = User(username=username, dob=dob)
+        db.session.add(new_user)
+        db.session.commit()
 
-    # Save data to DynamoDB
-    response = table.put_item(
-        Item={
-            'Username': username,
-            'DOB': dob.strftime('%Y-%m-%d')
-        }
-    )
-
-    return "Data saved successfully"
-
-@app.route('/view')
-def view():
-    # Retrieve data from DynamoDB
-    response = table.scan()
-    data = response.get('Items', [])
-
-    return render_template('view.html', data=data)
+        return f'Data saved for user: {username}, Date of Birth: {dob}'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(debug=True)
