a5ad1f9995d40a160b946bb8d9533bb952d9294f
saravananibus <152783206+saravananibus@users.noreply.github.com>
2023-12-18 19:30:38 +0530

    Update app.py

--- project/app.py
+++ project/app.py
@@ -5,7 +5,7 @@ import pymysql
 app = Flask(__name__)
 
 # Replace these with your database credentials
-DB_HOST = 'myrdsinstance.cyf3uod2jso1.ap-south-1.rds.amazonaws.com'
+DB_HOST = 'myrdsinstance-1.cyf3uod2jso1.ap-south-1.rds.amazonaws.com'
 DB_USER = 'admin'
 DB_PASSWORD = 'admin123'
 DB_NAME = 'mmss_mmss2'
